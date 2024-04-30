14d05938299bca57af71618cd82f868d2af3a5bf
karl-died <karl.diedrichsen@haw-hamburg.de>
2024-04-30 16:34:27 +0200

    initial commit

---
+++ src/main/java/com/example/aprg_projekt/AprgProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.aprg_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprgProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprgProjektApplication.class, args);
+	}
+
+}
