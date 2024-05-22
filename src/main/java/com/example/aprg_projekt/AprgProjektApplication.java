f7e325462583481af6da110becfaabd683970268
karl-died <karl.diedrichsen@haw-hamburg.de>
2024-05-22 09:04:59 +0200

    AprgProjektApplication extends SpringBootServletInitializer for deployment

--- src/main/java/com/example/aprg_projekt/AprgProjektApplication.java
+++ src/main/java/com/example/aprg_projekt/AprgProjektApplication.java
@@ -2,9 +2,11 @@ package com.example.aprg_projekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AprgProjektApplication {
+public class AprgProjektApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AprgProjektApplication.class, args);
